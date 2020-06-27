9ffab460525ed4152e257d618fb78e89ec46accd
Dona Paul <donapaul1996@cusat.ac.in>
2020-06-27 11:24:33 +0530

    Edited completion

--- Rassignment.R
+++ Rassignment.R
@@ -1,12 +1,12 @@
 makeMatrix <- function(x = matrix()) {
   inverse <- NULL
-  set <- function(y) {
+  set <- function(y) { ##this function set the matrix value
     x <<- y
     inverse <<- NULL
   }
   get <- function() x
   setinverse <- function(solve) inverse <<- solve
-  cacheSolve <- function() inverse
+  cacheSolve <- function() inverse ## This function create the inverse of matrix
   list(set = set, get = get,
        setinverse = setinverse,
        cacheSolve = cacheSolve)
@@ -15,7 +15,7 @@ makeMatrix <- function(x = matrix()) {
 makeCacheMatrix <- function(x, ...) {
   inverse <- x$cacheSolve()
   if(!is.null(inverse)) {
-    message("getting cached data")
+    message("getting cached data") ## If inverse of matrix is present in cache then it returns the matrix inverse
     return(inverse)
   }
   data <- x$get()
